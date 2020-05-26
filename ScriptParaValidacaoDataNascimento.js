77a3cd2b8807f6b9cb0ca4561b2fd68321dfe535
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-05-26 14:21:29 -0300

    Script para validação data de nascimento

---
+++ ScriptParaValidacaoDataNascimento.js
@@ -0,0 +1,30 @@
+function run(data) {
+        // Ex: 10/01/1985
+        var regex = "^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$";
+        var dtArray = data.split("/");
+        var now = new Date;
+        if (dtArray == null)
+            return false;
+        // Checando se está no formato dd/mm/yyyy.
+        var dtDay= dtArray[0];
+        var dtMonth = dtArray[1];
+        var dtYear = dtArray[2];
+        if (dtMonth < 1 || dtMonth > 12)
+            return false;
+        else if (dtDay < 1 || dtDay> 31)
+            return false;
+        else if ((dtMonth==4 || dtMonth==6 || dtMonth==9 || dtMonth==11) && dtDay ==31)
+            return false;
+        else if(dtYear > now.getFullYear())
+            return false;
+        else if (dtMonth == 2)
+        {
+            var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+            if (dtDay> 29 || (dtDay ==29 && !isleap))
+                return false;
+        }
+  
+        return true;
+    }
+
+console.log(run('29/02/1991'));
