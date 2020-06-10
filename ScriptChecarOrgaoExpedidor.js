6d5fde3c0eeede66c5871215157ce58c7251685b
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-06-10 13:27:25 -0300

    Correção de Script

--- ScriptChecarOrgaoExpedidor.js
+++ ScriptChecarOrgaoExpedidor.js
@@ -2,7 +2,7 @@
 function run(orgaoExpedidor) {
 
   var orgaoExpedidor = orgaoExpedidor.toUpperCase();
-  var orgaos = ['SSP', 'DETRAM', 'OAB', 'CARTÓRIO CIVIL ','CARTORIO CIVIAL','POLICIA FEDERAL', 'PF', 'POLÍCIA FEDERAL', 'ABNC', 'DPF', 'CGPI', 'CGPMAF', 'CNIG', 'COREN', 'CNT', 'CRA', 'CRAS', 'OMB', 'POF', 'DPF', 'POM', 'SNJ', 'SJS', 'SDS', 'SES', 'EST'];
+  var orgaos = ['SSP', 'DETRAN', 'OAB', 'CARTÓRIO CIVIL ','CARTORIO CIVIAL','POLICIA FEDERAL', 'PF', 'POLÍCIA FEDERAL', 'ABNC', 'DPF', 'CGPI', 'CGPMAF', 'CNIG', 'COREN', 'CNT', 'CRA', 'CRAS', 'OMB', 'POF', 'DPF', 'POM', 'SNJ', 'SJS', 'SDS', 'SES', 'EST'];
   var orgao_valido = orgaoExpedidor;
 
   if (orgaos.indexOf(orgao_valido) > -1) {
