2d3bd9afafe59dce0e9bd05e796e1f3f1885dee7
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-06-23 17:05:42 -0300

    Script para verificar se uma string é > 200

---
+++ ScriptParaContarQuantCarc.js
@@ -0,0 +1,12 @@
+/*
+    Script para contar a quantidade de caracteres em uma string
+*/
+
+function rum(observacao) {
+
+    if (observacao > 200) {
+        return false;
+    }
+    return true;
+    
+}
