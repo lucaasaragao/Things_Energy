d89842a3f95d467acf98dd447906400c0b8b3ca1
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-07-29 13:40:43 -0300

    Correção de script, obejtos não estava sendo acessado corretamente

--- ScriptParaRecuperarStatusDeUmObjeto.js
+++ ScriptParaRecuperarStatusDeUmObjeto.js
@@ -6,6 +6,7 @@ function run(corporespostaucnaoautenticado,taxasPrazosReligacao) {
     var valorNormal = "";
     var valorUrgencia = "";
     var prazo = "";
+    var prazoUrgencia = "";
     var mensagens = "";
 
     // Recuperando os dados da intalação
@@ -16,76 +17,76 @@ function run(corporespostaucnaoautenticado,taxasPrazosReligacao) {
 Monofásico
 */
     if(tipoligacao == "MONOFASICO" & areaRural == false){
-       
         valorNormal = taxasEPrazos.valorReligacaoNormal[0].valor;
-        valorUrgencia = "Infelizmente você não pode solicitar religação com urgência"
+        valorUrgencia = taxasEPrazos.valorReligacaoUrgencia[0].valor
         prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorUrbanaNormal;
+        prazoUrgencia = taxasEPrazos.prazoReligacao.religacaoSemMedidorUrbanaUrgencia;
         mensagens = "A taxa para solicitar a religação varia de acordo com o tipo da sua ligação.\n \n" 
-            + `No seu caso sua ligação é Monofásico.\nA taxa para ligação normal é: R$${valorNormal}\n` 
-            + `${valorUrgencia}\n` 
-            + `Esse é o prazo para religação na a área urbana: ${prazo}`;
+            + `A sua ligação é do tipo monofásica.\nA taxa para religação normal é: ${valorNormal}\n` 
+            + `A taxa para religação urgente é: ${valorUrgencia}\n` 
+            + `Esse é o prazo para religação normal na área urbana: ${prazo}`
+            + `Esse é o prazo para religação urgente na área urbana: ${prazoUrgencia}`;
 
-    }else if(tipoligacao == "MONOFASICO" & areaRural == true){
-        
+    }else if(tipoligacao == "MONOFASICO" & areaRural == true){ 
         valorNormal = taxasEPrazos.valorReligacaoNormal[0].valor;
-        valorUrgencia = "Infelizmente você não pode solicitar religação com urgência"
+        valorUrgencia = taxasEPrazos.valorReligacaoUrgencia[0].valor
         prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorRuralNormal;
+        prazoUrgencia = taxasEPrazos.prazoReligacao.religacaoSemMedidorRuralUrgencia;
         mensagens = "A taxa para solicitar a religação varia de acordo com o tipo da sua ligação.\n \n" 
-            + `No seu caso sua ligação é Monofásico.\nA taxa para ligação normal é: R$${valorNormal}\n` 
-            + `${valorUrgencia}\n` 
-            + `Esse é o prazo para religação na a área urbana: ${prazo}`;
+            + `A sua ligação é do tipo monofásica.\nA taxa para religação normal é: ${valorNormal}\n` 
+            + `A taxa para religação urgente é: ${valorUrgencia}\n` 
+            + `Esse é o prazo para religação normal na a área rural: ${prazo}`
+            + `Esse é o prazo para religação urgente na a área rural: ${prazoUrgencia}`;
 /*
 Bifasico
 */
     }else if(tipoligacao == "BIFASICO" & areaRural == false){
-       
         valorNormal = taxasEPrazos.valorReligacaoNormal[1].valor;
-        valorUrgencia = "Infelizmente você não pode solicitar religação com urgência"
+        valorUrgencia = taxasEPrazos.valorReligacaoUrgencia[1].valor
         prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorUrbanaNormal;
+        prazoUrgencia = taxasEPrazos.prazoReligacao.religacaoSemMedidorUrbanaUrgencia;
         mensagens = "A taxa para solicitar a religação varia de acordo com o tipo da sua ligação.\n \n" 
-            + `No seu caso sua ligação é Monofásico.\nA taxa para ligação normal é: R$${valorNormal}\n` 
-            + `${valorUrgencia}\n` 
-            + `Esse é o prazo para religação na a área urbana: ${prazo}`;
+            + `A sua ligação é do tipo bifásica.\nA taxa para religação normal é: ${valorNormal}\n` 
+            + `A taxa para religação urgente é: ${valorUrgencia}\n` 
+            + `Esse é o prazo para religação normal na a área urbana: ${prazo}`
+            + `Esse é o prazo para religação urgente na a área urbana: ${prazoUrgencia}`;
 
-    }else if(tipoligacao == "BIFASICO" & areaRural == true){
-        
+    }else if(tipoligacao == "BIFASICO" & areaRural == true){ 
         valorNormal = taxasEPrazos.valorReligacaoNormal[1].valor;
-        valorUrgencia = "Infelizmente você não pode solicitar religação com urgência"
+        valorUrgencia = taxasEPrazos.valorReligacaoUrgencia[1].valor
         prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorRuralNormal;
+        prazoUrgencia = taxasEPrazos.prazoReligacao.religacaoSemMedidorRuralUrgencia;
         mensagens = "A taxa para solicitar a religação varia de acordo com o tipo da sua ligação.\n \n" 
-            + `No seu caso sua ligação é Monofásico.\nA taxa para ligação normal é: R$${valorNormal}\n` 
-            + `${valorUrgencia}\n` 
-            + `Esse é o prazo para religação na a área urbana: ${prazo}`;
+            + `A sua ligação é do tipo bifásica.\nA taxa para religação normal é: ${valorNormal}\n` 
+            + `A taxa para religação urgente é: ${valorUrgencia}\n` 
+            + `Esse é o prazo para religação normal na a área rural: ${prazo}`
+            + `Esse é o prazo para religação urgente na a área rural: ${prazoUrgencia}`;
     /*
 Trifásico
 */
     }else if(tipoligacao == "TRIFASICO" & areaRural == false){
-       
-        valorNormal = taxasEPrazos.valorReligacaoNormal[1].valor;
-        valorUrgencia = "Infelizmente você não pode solicitar religação com urgência"
-        prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorUrbanaNormal;
-        mensagens = "A taxa para solicitar a religação varia de acordo com o tipo da sua ligação.\n \n" 
-            + `No seu caso sua ligação é Monofásico.\nA taxa para ligação normal é: R$${valorNormal}\n` 
-            + `${valorUrgencia}\n` 
-            + `Esse é o prazo para religação na a área urbana: ${prazo}`;
-
-    }else if(tipoligacao == "TRIFASICO" & areaRural == true){
-        
         valorNormal = taxasEPrazos.valorReligacaoNormal[2].valor;
-        valorUrgencia = "Infelizmente você não pode solicitar religação com urgência"
-        prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorRuralNormal;
+        valorUrgencia = taxasEPrazos.valorReligacaoUrgencia[2].valor
+        prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorUrbanaNormal;
+        prazoUrgencia = taxasEPrazos.prazoReligacao.religacaoSemMedidorUrbanaUrgencia;
         mensagens = "A taxa para solicitar a religação varia de acordo com o tipo da sua ligação.\n \n" 
-            + `No seu caso sua ligação é Monofásico.\nA taxa para ligação normal é: R$${valorNormal}\n` 
-            + `${valorUrgencia}\n` 
-            + `Esse é o prazo para religação na a área urbana: ${prazo}`;
+            + `A sua ligação é do tipo trifásica.\nA taxa para religação normal é: ${valorNormal}\n` 
+            + `A taxa para religação urgente é: ${valorUrgencia}\n` 
+            + `Esse é o prazo para religação normal na a área urbana: ${prazo}`
+            + `Esse é o prazo para religação urgente na a área urbana: ${prazoUrgencia}`;
+
+    }else if(tipoligacao == "TRIFASICO" & areaRural == true){   
+        valorNormal = taxasEPrazos.valorReligacaoNormal[2].valor;
+        valorUrgencia = taxasEPrazos.valorReligacaoUrgencia[2].valor
+        prazo = taxasEPrazos.prazoReligacao.religacaoSemMedidorRuralNormal;
+        prazoUrgencia = taxasEPrazos.prazoReligacao.religacaoSemMedidorRuralUrgencia;
+        mensagens = "A taxa para solicitar a religação varia de acordo com o tipo da sua ligação.\n \n" 
+            + `A sua ligação é do tipo trifásica.\nA taxa para religação normal é: ${valorNormal}\n` 
+            + `A taxa para religação urgente é: ${valorUrgencia}\n` 
+            + `Esse é o prazo para religação normal na a área rural: ${prazo}`
+            + `Esse é o prazo para religação urgente na a área rural: ${prazoUrgencia}`;
     }
     
     
     return mensagens
 }
-
-
-
-corporespostaucnaoautenticado
-taxasPrazosReligacao
-
